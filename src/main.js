e363dab0a9db1e4721f1aaba9f23dff258e33989
Aaron <kesslerawkessler@gmail.com>
2023-03-31 10:44:19 -0600

    Quotes added

--- src/main.js
+++ src/main.js
@@ -5,22 +5,28 @@ $(document).ready(() => {
   const $quoteText = $('#text');
   const $authorText = $('#author');
 //quote variables
-const $quoteOne = '1';
-const $quoteTwo = '2';
-const $quoteThree = '3';
-const $quoteFour = '4';
-const $quoteFive = '5';
-const $quoteSix = '6';
+const $quoteOne = "All that we are is the result of what we have thought.";
+const $quoteTwo = "Nothing can stop the man with the right mental attitude from achieving his goal; nothing on earth can help the man with the wrong mental attitude.";
+const $quoteThree = "Our truest life is when we are in dreams awake.";
+const $quoteFour = "Whether you think you can, or you think you can't, you're right.";
+const $quoteFive = "When I let go of what I am, I become what I might be.";
+const $quoteSix = "Muddy water is best cleared by leaving it alone.";
+const $quoteSeven = "When the whole world is silent, even one voice becomes powerful.";
+const $quoteEight = "If you obey all the rules, you miss all the fun.";
+const $quoteNine =  "Letting go gives us freedom, and freedom is the only condition for happiness. If, in our heart, we still cling to anything - anger, anxiety, or possessions - we cannot be free.";
 //author variables
-const $authorOne = 'one';
-const $authorTwo = 'two';
-const $authorThree = 'three';
-const $authorFour = 'four';
-const $authorFive = 'five';
-const $authorSix = 'six';
+const $authorOne = "Buddha";
+const $authorTwo = "Thomas Jefferson";
+const $authorThree = "Henry David Thoreau";
+const $authorFour = "Henry Ford";
+const $authorFive = "Lao Tzu";
+const $authorSix = "Alan Watts";
+const $authorSeven = "Malala Yousafzai";
+const $authorEight = "Katherine Hepburn";
+const $authorNine = "Thich Nhat Hanh"; 
 
 //quotes Array
-const $quotes = [$quoteOne, $quoteTwo, $quoteThree, $quoteFour, $quoteFive, $quoteSix];
+const $quotes = [$quoteOne, $quoteTwo, $quoteThree, $quoteFour, $quoteFive, $quoteSix, $quoteSeven, $quoteEight, $quoteNine];
   //handlers
   $newQuote.on('click', () => {
     //randomize quotes
@@ -44,6 +50,15 @@ const $quotes = [$quoteOne, $quoteTwo, $quoteThree, $quoteFour, $quoteFive, $quo
         break; 
       case $quoteSix:
         var $author = $authorSix;
+        break;
+      case $quoteSeven:
+        var $author = $authorSeven;
+        break;
+      case $quoteEight:
+        var $author = $authorEight;
+        break;
+      case $quoteNine:
+        var $author = $authorNine;
         break;
       default:
         var $author = '';
